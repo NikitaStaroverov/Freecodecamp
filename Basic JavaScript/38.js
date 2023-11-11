fed4df8c51bb3cd308054512eb31f3e469a73fb2
NikitaStaroverov <starovierov98@mail.ru>
2023-11-11 23:59:07 +0300

    the third day

---
+++ Basic JavaScript/38.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = ['my name', 4, 'big', 2]
---
+++ Basic JavaScript/39.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+	['my name', 25],
+	['Nikita', 98],
+]
