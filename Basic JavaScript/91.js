733fb465583fc9ca8a1ad8b04b82d0df3eccbfde
NikitaStaroverov <starovierov98@mail.ru>
2023-11-15 23:57:23 +0300

    the seventh day

---
+++ Basic JavaScript/91.js
@@ -0,0 +1,10 @@
+function checkObj(obj, checkProp) {
+	// Only change code below this line
+	if (obj.hasOwnProperty(checkProp) == true) {
+		return obj[checkProp]
+	} else if (obj.hasOwnProperty(checkProp) == false) {
+		return 'Not Found'
+	}
+
+	// Only change code above this line
+}
---
+++ Basic JavaScript/92.js
@@ -0,0 +1,15 @@
+const myMusic = [
+	{
+		artist: 'Billy Joel',
+		title: 'Piano Man',
+		release_year: 1973,
+		formats: ['CD', '8T', 'LP'],
+		gold: true,
+	},
+	{
+		artist: 'NikStar',
+		title: 'Sounds of nature',
+		release_year: 2023,
+		formats: ['mp3', 'youtube'],
+	},
+]
