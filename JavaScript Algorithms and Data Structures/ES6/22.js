781051883ccf045630444ad6ef3da71af045949c
NikitaStaroverov <starovierov98@mail.ru>
2023-12-06 23:31:23 +0300

    2 day ES6

---
+++ JavaScript Algorithms and Data Structures/ES6/22.js
@@ -0,0 +1,5 @@
+import { uppercaseString, lowercaseString } from './21.js'
+// Only change code above this line
+
+uppercaseString('hello')
+lowercaseString('WORLD!')
---
+++ JavaScript Algorithms and Data Structures/ES6/25.js
@@ -0,0 +1,4 @@
+import subtract from './24.js'
+// Only change code above this line
+
+subtract(7, 4)
